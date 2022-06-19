e5ed739da7d6cddbcc03586c9c7370a119773839
Simon Beddig <simon.beddig@googlemail.com>
2022-06-19 22:20:30 +0200

    add cfn and sam to local setup

--- aws/install.sh
+++ aws/install.sh
@@ -19,6 +19,15 @@ install() {
     curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
     dpkg -i session-manager-plugin.deb
     rm -f session-manager-plugin.deb
+
+    wget https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
+    unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
+    ./sam-installation/install
+
+    rm -rf sam-installation
+    rm -f aws-sam-cli-linux-x86_64.zip
+
+    pip install cloudformation-cli cloudformation-cli-java-plugin cloudformation-cli-go-plugin cloudformation-cli-python-plugin cloudformation-cli-typescript-plugin
   fi
 }
 
